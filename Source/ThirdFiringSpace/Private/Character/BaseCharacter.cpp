35d4e438467c575e82afa45a46bf4365d6203371
like-coder <www.821765296@qq.com>
2025-02-07 23:25:30 +0800

    add ak47 montation

--- Source/ThirdFiringSpace/Private/Character/BaseCharacter.cpp
+++ Source/ThirdFiringSpace/Private/Character/BaseCharacter.cpp
@@ -8,7 +8,7 @@ ABaseCharacter::ABaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	SetReplicates(true);
 }
 
 // Called when the game starts or when spawned
