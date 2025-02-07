d3ec75166537ce6dce9d023dea8889e250d4547d
like-coder <81460494+like-coder@users.noreply.github.com>
2025-02-07 14:51:37 +0800

    add ThirdFiringSpace

---
+++ Source/ThirdFiringSpace/Public/GamePlay/ThirdFiringSpacePlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ThirdFiringSpacePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDFIRINGSPACE_API AThirdFiringSpacePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
